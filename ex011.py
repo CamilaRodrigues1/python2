46fd311d5502908dd2a38d07cca250c6f84e5125
CamilaRodrigues1 <133536718+CamilaRodrigues1@users.noreply.github.com>
2024-03-05 14:00:01 -0300

    exercicios
    1

---
+++ ex011.py
@@ -0,0 +1,4 @@
+lar = float(input('Largura da parede: '))
+alt = float(input('Altura da parede: '))
+print(f'Sua parede tem a dimesão de {lar}X{alt} e sua area é de  {lar*alt}m².')
+print(f'Você vai precisar de {alt*lar/2:.2f} litros de tinta.')
---
+++ ex012.py
@@ -0,0 +1,2 @@
+pro = float(input('Qual é o valor do produto?R$ '))
+print(f'O produto tem o valor de R$ {pro:.2f} e com desconto de 5% ficará por R$ {pro-(pro*5/100):.2f}')
