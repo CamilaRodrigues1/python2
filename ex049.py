22752817f01a1a4875ea8dac7cfbac02e252271c
CamilaRodrigues1 <133536718+CamilaRodrigues1@users.noreply.github.com>
2024-03-23 18:53:48 -0300

    exercicios

---
+++ ex049.py
@@ -0,0 +1,8 @@
+print('='*30)
+print('           Tabuada')
+print('='*30)
+num = int(input('qual é o número? '))
+for c in range(1,11):
+    print(f'{num} x {c:2} = {num*c}')
+print('='*30)
+print('fim..................')
---
+++ ex050.py
@@ -0,0 +1,8 @@
+soma = cont = 0
+
+for c in range(1,7):
+    num = int(input(f'Digite o {c}° número: '))
+    if num % 2 == 0:
+        cont += 1
+        soma += num
+print(f'A soma de todos {cont} números pares são {soma}')
