0f8724cefe45f949deab3b45017cbfc15519e998
CamilaRodrigues1 <133536718+CamilaRodrigues1@users.noreply.github.com>
2024-03-27 11:07:50 -0300

    exercicios

--- ex051.py
+++ ex051.py
@@ -1,1 +1,6 @@
-f
+termo = int(input('Primeiro termo: '))
+razao = int(input('Razâo: '))
+decimo = termo + (10 - 1) * razao
+for c in range(termo, decimo, razao):
+    print(c, end=' -> ')
+print('acabou')
