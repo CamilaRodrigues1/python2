55868e385eff3db4dc269ac08db22d6de8d36985
CamilaRodrigues1 <133536718+CamilaRodrigues1@users.noreply.github.com>
2024-03-27 22:53:56 -0300

    exercicios

---
+++ ex059.py
@@ -0,0 +1,31 @@
+num1 = int(input('Primeiro valor: '))
+num2 = int(input('Segundo valor: '))
+resp = 0
+while resp != 5:
+    print('''    [1] somar
+    [2] multiplicar
+    [3] maior
+    [4] novos numeros
+    [5] sair do programa''')
+    resp = int(input('Qual é a sua opção? '))
+    if resp == 1:
+        print('=-' * 30)
+        print(f'O resultado de {num1} + {num2} = {num1+num2}')
+        print('=-'*30)
+    elif resp == 2:
+        print('=-' * 30)
+        print(f'O resultado de {num1} * {num2} = {num1*num2}')
+        print('=-'*30)
+    elif resp == 3:
+        maior = num1
+        if num1 > num2:
+            maior = num1
+        if num1 < num2:
+            maior = num2
+        print('=-' * 30)
+        print(f'O numero {maior} é maior')
+        print('=-'*30)
+    elif resp == 4:
+        num1 = int(input('Primeiro valor: '))
+        num2 = int(input('Segundo valor: '))
+print('fim do programa')
