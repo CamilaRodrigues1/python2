fc27e3d1f2ed89388ba83240f8cd4f2a5bffd8b7
CamilaRodrigues1 <133536718+CamilaRodrigues1@users.noreply.github.com>
2024-04-02 10:33:09 -0300

    Update ex070.py

---
+++ ex070.py
@@ -0,0 +1,31 @@
+print('=-'*30)
+print('lojas compra mas não leva')
+print('=-'*30)
+cont = s = promil = menor = nome = 0
+resp = ' '
+while True:
+    print('=-'*30)
+    produto = str(input('NOME DO PRODUTO: ')).strip().upper()
+    preco = float(input('VALOR DO PRODUTO:R$  '))
+    resp = str(input('QUER CONTINUAR? [S/N] ')).strip().upper()[0]
+    s += preco
+    cont += 1
+    if cont == 1:
+        menor = preco
+    if preco < menor:
+        menor = preco
+        produto = menor
+        nome = produto
+    if preco >= 1000:
+        promil += 1
+    if resp in 'Nn':
+        break
+print('=-'*30)
+print('VALOR DA SUAS COMPRAS')
+print('=-'*30)
+print(f'''O total das compra é de R$ {s:.2f}
+Foram comprados {promil} produtos que custa mais de R$1000.00.
+O produto mais barato é o {nome} que custou R$ {menor:.2f}''')
+
+
+
