328d4f0a62bbdac6e0bc42ca5f6d487c1d7f3d13
CamilaRodrigues1 <133536718+CamilaRodrigues1@users.noreply.github.com>
2024-05-23 11:44:42 -0300

    exercicios

---
+++ ex096.py
@@ -0,0 +1,18 @@
+'''def soma(largu, comp):
+    s = largu * comp
+    print(f'a area de um terreno {largu}x{comp} é de {s}m²')
+
+
+l = float(input('LARGURA (m): '))
+c = float(input('COMPRIMENTO (m): '))
+soma(l, c)'''
+
+#meu codigo abaixo
+def soma():
+    s = l * c
+    print(f'A area de um terreno {l}x{c} é de {s}m².')
+
+
+l = float(input('LARGURA (m): '))
+c = float(input('COMPRIMENTO (m): '))
+soma()
